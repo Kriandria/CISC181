d7818106711cc320c412eaee6c29c8828a61ac3b
NotandorSS <jkmorris@udel.edu>
2016-02-17 13:13:00 -0500

    Update lab1.java

--- lab1.java
+++ lab1.java
@@ -1,3 +1,19 @@
 // Dylan Leh, Jessica Morris, ________, ________
 // Feb 10, 2016
 // Cisc 181
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double deposit = 10000-2642;
+		double total = 1454485.55;
+		for (int i = 1; i < 241; i++){
+			total += total*(.02/12);
+			total -= 10000-2642;
+			System.out.println(i +": " + total);
+		}
+		
+	}
+
+}
