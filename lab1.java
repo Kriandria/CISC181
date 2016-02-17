71650a33a45bc09342c84660af1d37c3cea9bd68
NotandorSS <jkmorris@udel.edu>
2016-02-17 15:56:36 -0500

    Update lab1.java

--- lab1.java
+++ lab1.java
@@ -6,14 +6,26 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		double deposit = 10000-2642;
-		double total = 1454485.55;
-		for (int i = 1; i < 241; i++){
-			total += total*(.02/12);
-			total -= 10000-2642;
-			System.out.println(i +": " + total);
-		}
-		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Please enter your required income: ");
+		float RequiredIncome = scan.nextFloat();
+		System.out.print("Please enter your monthly SS income: ");
+		float MonthlySSIncome = scan.nextFloat();
+		System.out.print("Please enter your annual retirment rate (no percent sign): ");
+		byte AnnualRetirement = scan.nextByte();
+		System.out.print("Please enter how many years you plan to be retired: ");
+		byte RetiredYears = scan.nextByte();
+		System.out.print("Please enter your annual savings rate (no percent sign): ");
+		byte AnnualReturn = scan.nextByte();
+		System.out.print("Please enter how many more years you plan on working: ");
+		byte numberOfYears = scan.nextByte();
+		scan.close();
+		System.out.println();
+        double TotalSaving = (RequiredIncome-MonthlySSIncome)*((1-(1/(Math.pow(1+(AnnualRetirement/100.0)/12,RetiredYears*12 )))))/((AnnualRetirement/100.0)/12);
+        double SaveMonthly = TotalSaving*(((AnnualReturn/100.0)/12)/((Math.pow(1+(AnnualReturn/100.0)/12, numberOfYears*12))-1));
+        System.out.println("How much to save monthly: "+ String.format("%.5g%n", SaveMonthly));
+        
 	}
 
 }
+
